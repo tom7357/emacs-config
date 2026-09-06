fbf4ac386c6dfaa04d447a77c66af432b52f6498
tom <675864319@qq.com>
2026-09-06 12:54:22 +0900

    Remove smart-mode-line

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,36 +1,34 @@
 ;;; init-ui.el --- UI settings -*- lexical-binding: t; -*-
-;; gruvbox-theme
-(use-package gruvbox-theme 
-    :init (load-theme 'gruvbox-dark-soft t))
 
-(use-package smart-mode-line 
-    :init 
-    (setq sml/no-confirm-load-theme t) 
-    (setq sml/theme 'respectful) 
-    (sml/setup))
+;; Theme
+(use-package gruvbox-theme
+  :config
+  (load-theme 'gruvbox-dark-soft t))
 
+;; Fonts
 (use-package emacs
   :ensure nil
   :if (display-graphic-p)
   :config
   (if *is-windows*
       (progn
-        ;; 英文/代码字体
+        ;; English / code font
         (set-face-attribute 'default nil
                             :font "Consolas"
                             :height 105)
 
-        ;; 中文、日文
+        ;; Chinese / Japanese fonts
         (dolist (charset '(kana han cjk-misc bopomofo))
           (set-fontset-font t charset
                             (font-spec :family "Microsoft YaHei")))
 
-        ;; 符号
+        ;; Symbols
         (set-fontset-font t 'symbol
                           (font-spec :family "Segoe UI Symbol")))
     (set-face-attribute 'default nil
                         :font "Source Code Pro"
                         :height 110)))
 
-
 (provide 'init-ui)
+
+;;; init-ui.el ends here
