6fe411d97b278bfa4306e7131b59495cf1500a22
tom <675864319@qq.com>
2026-09-06 13:39:40 +0900

    Add Flymake navigation keybindings

--- lisp/init-kbd.el
+++ lisp/init-kbd.el
@@ -1,4 +1,7 @@
 ;;; init-kbd.el --- Keybindings -*- lexical-binding: t; -*-
 (use-package emacs :config (defalias 'yes-or-no-p 'y-or-n-p))
 
+(global-set-key (kbd "M-]") #'flymake-goto-next-error)
+(global-set-key (kbd "M-[") #'flymake-goto-prev-error)
+
 (provide 'init-kbd)
