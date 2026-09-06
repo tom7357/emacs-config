ea0a6e92abd03f0b8e619c0d96db2021712f48ba
tom <675864319@qq.com>
2026-09-06 13:42:43 +0900

    Add Tree-sitter configuration

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -23,6 +23,38 @@
   :custom
   (which-key-idle-delay 0.5))
 
+(use-package treesit
+  :ensure nil
+  :when (and (fboundp 'treesit-available-p)
+             (treesit-available-p))
+  :custom
+  (treesit-font-lock-level 4)
+  :config
+  (setq treesit-language-source-alist
+        '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
+          (c . ("https://github.com/tree-sitter/tree-sitter-c"))
+          (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
+          (java . ("https://github.com/tree-sitter/tree-sitter-java"))
+          (json . ("https://github.com/tree-sitter/tree-sitter-json"))
+          (python . ("https://github.com/tree-sitter/tree-sitter-python"))
+          (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))))
+
+  (add-to-list 'major-mode-remap-alist
+               '(sh-mode . bash-ts-mode))
+  (add-to-list 'major-mode-remap-alist
+               '(c-mode . c-ts-mode))
+  (add-to-list 'major-mode-remap-alist
+               '(c++-mode . c++-ts-mode))
+  (add-to-list 'major-mode-remap-alist
+               '(java-mode . java-ts-mode))
+  (add-to-list 'major-mode-remap-alist
+               '(js-json-mode . json-ts-mode))
+  (add-to-list 'major-mode-remap-alist
+               '(python-mode . python-ts-mode))
+  (add-to-list 'major-mode-remap-alist
+               '(js-mode . js-ts-mode)))
+
+
 (provide 'init-package)
 
 ;;; init-package.el ends here
