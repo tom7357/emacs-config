9dc0c26d651bd7575324842bed07c27746027b9d
tom <675864319@qq.com>
2026-09-06 13:13:32 +0900

    Add company completion

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -1,3 +1,14 @@
 ;;; init-package.el --- Package configuration -*- lexical-binding: t; -*-
 
+(use-package company
+  :ensure t
+  :hook (after-init . global-company-mode)
+  :custom
+  (company-idle-delay 0.2)
+  (company-minimum-prefix-length 2)
+  (company-selection-wrap-around t)
+  (company-tooltip-limit 10))
+
 (provide 'init-package)
+
+;;; init-package.el ends here
